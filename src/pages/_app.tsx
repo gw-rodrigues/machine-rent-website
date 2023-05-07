c15dbcc61c0b6ee1d0ae4b791c392e0cbd30dfaa
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2023-05-07 13:41:33 +0200

    feat: add app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+import { Navigation } from '@/components/layout/Navigation'
+import '@/styles/globals.css'
+import { Footer } from '@/components/layout/Footer'
+import { Header } from '@/components/layout/Header'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`w-full min-h-screen ${inter.className}`}>
+      <Navigation />
+      <Header />
+      <main className="py-8 flex flex-col gap-8">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  )
 }
